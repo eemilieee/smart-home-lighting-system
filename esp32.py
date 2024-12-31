a27c358c54d81ad3565b20dbf22e920249ee3a8b
Arpășanu Emilia-Oana <94164681+eemilieee@users.noreply.github.com>
2024-12-31 16:54:59 +0200

    Adaptat trimitere date prin socket

--- esp32.py
+++ esp32.py
@@ -1,7 +1,10 @@
 from machine import ADC, Pin
 import time
 import network
-import requests
+import urequests
+import socket
+import ssl
+import ujson
 
 # date despre wi-fi
 SSID = "wifi-arp"
@@ -10,7 +13,7 @@ PASSWORD = "acasa123"
 # date despre server-ul flask
 SERVER_IP = "192.168.0.243"
 SERVER_PORT = 5000
-URL = f"http://{SERVER_IP}:{SERVER_PORT}/submit"
+URL = f"https://{SERVER_IP}:{SERVER_PORT}/submit"
 
 class LDR:
 
@@ -37,10 +40,35 @@ def connect_to_wifi(ssid, password):
     return wlan
 
 def send_post_request(url, data):
-    response = requests.post(url, json=data)
+    response = urequests.post(url, json=data)
     print('Response status code:', response.status_code)
     print('Response text:', response.text)
     response.close()
+    
+def connect_to_socket():
+    addr = socket.getaddrinfo(SERVER_IP, SERVER_PORT)[0][-1]
+
+    sock = socket.socket()
+    sock.connect(addr)
+    sock = ssl.wrap_socket(sock)
+    
+    return sock
+    
+def send_to_socket(sock, data):
+    json_data = ujson.dumps(data)
+    packet = (
+        f"POST /submit HTTP/1.1\r\n"
+        f"Host: {SERVER_IP}:{SERVER_PORT}\r\n"
+        f"Content-Type: application/json\r\n"
+        f"Content-Length: {len(json_data)}\r\n"
+        f"\r\n"
+        f"{json_data}"
+    )
+
+    sock.write(packet.encode('utf-8'))
+    resp = sock.readline().decode('utf-8').strip()
+    print(resp)
+    
 
 wlan = connect_to_wifi(SSID, PASSWORD)
 
@@ -53,6 +81,10 @@ if wlan.isconnected():
         print('Light intensity value = {}'.format(value))
         
         payload = {"light_intensity": int(value)}
-        send_post_request(URL, payload)
+        # send_post_request(URL, payload)
 
-        time.sleep(60)
+        sock = connect_to_socket()
+        send_to_socket(sock, payload)
+        sock.close()
+
+        time.sleep(1)
